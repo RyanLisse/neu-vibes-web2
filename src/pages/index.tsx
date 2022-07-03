6fcf7acef864834f6047253c1ea0c4dfd6525de2
RyanLisse <ryan.lisse@gmail.com>
2022-07-03 11:19:03 +0200

    added components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,39 +10,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col items-center justify-center w-1/2 min-h-screen mx-auto">
-        <h1 className="font-extrabold text-center text-7xl">
-          Create <span className="text-blue-500">T3</span> App
+        <h1 className="font-black text-center uppercase text-amber-300 text-7xl">
+          Neu Vibez
         </h1>
 
-        <div className="w-fit">
-          <h3 className="mt-4 text-3xl">This Stack uses:-</h3>
-          <ul className="self-start text-xl underline list-disc">
-            <li>
-              <a href="https://nextjs.org" target="_blank" rel="noreferrer">
-                Next.js
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://tailwindcss.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                TailwindCSS
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://typescriptlang.org"
-                target="_blank"
-                rel="noreferrer"
-              >
-                TypeScript
-              </a>
-            </li>
-          </ul>
-          <div id="trpcUsage" />
-        </div>
+
+
       </div>
     </>
   );
