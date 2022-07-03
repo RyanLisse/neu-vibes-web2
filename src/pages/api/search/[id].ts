13f144da706c648542c426ae73296d64479f9b87
RyanLisse <ryan.lisse@gmail.com>
2022-07-03 17:59:04 +0200

    addded more stings in API

--- src/pages/api/search/[id].ts
+++ src/pages/api/search/[id].ts
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'GET') {
     const { id } = req.query;
 
-    const videosQuery = searchPostsQuery(id);
+    const videosQuery = searchPostsQuery("id")
 
     const videos = await client.fetch(videosQuery);
 
--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -5,7 +5,7 @@ import { client } from '../../utils/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const data = await client.fetch(allUsersQuery());
-  
+
   if(data) {
     res.status(200).json(data);
   } else {
